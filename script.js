bdabff9dfe149d365905143d99e3898f005e685c
goran <goranaviani@gmail.com>
2018-06-12 12:50:46 +0200

    fix seconds hand by removing transition. Added comments

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 function runTheClock() {
+    //Getting current date from browser
     var date = new Date();
-    console.log(date);
+    //Getting hour,min,sec
     let hr = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
-    console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
+    //Positioning by 360 degrees
     let hrPosition = (hr*360/12)+(min*(360/60)/12);
     let minPosition = (min*360/60)+(sec*(360/60)/60);
     let secPosition = sec*360/60;
 
+    //setting hands of clock based on degrees
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
     SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
